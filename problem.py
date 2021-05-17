339d36d921458b50e6ac33cbf818d48648f2fd09
maciejdomagala <mdomagala80@gmail.com>
2021-05-17 16:08:47 +0200

    a

--- problem.py
+++ problem.py
@@ -20,13 +20,26 @@ class GenerationProblem(Problem):
             generated = self.generator.generate(
                 ls, minibatch=self.config.batch_size)
             sim = self.generator.clip_similarity(generated).cpu().numpy()
-            if self.config.problem_args["n_obj"] == 2 and self.config.use_discriminator:
-                dis = self.generator.discriminate(
-                    generated, minibatch=self.config.batch_size)
-                hinge = torch.relu(1 - dis)
-                hinge = hinge.squeeze(1).cpu().numpy()
-                out["F"] = np.column_stack((-sim, hinge))
-            else:
-                out["F"] = -sim
 
+            out["F"] = -sim
             out["G"] = np.zeros((x.shape[0]))
+
+    # original
+    # def _evaluate(self, x, out, *args, **kwargs):
+    #     ls = self.config.latent(self.config)
+    #     ls.set_from_population(x)
+
+    #     with torch.no_grad():
+    #         generated = self.generator.generate(
+    #             ls, minibatch=self.config.batch_size)
+    #         sim = self.generator.clip_similarity(generated).cpu().numpy()
+    #         if self.config.problem_args["n_obj"] == 2 and self.config.use_discriminator:
+    #             dis = self.generator.discriminate(
+    #                 generated, minibatch=self.config.batch_size)
+    #             hinge = torch.relu(1 - dis)
+    #             hinge = hinge.squeeze(1).cpu().numpy()
+    #             out["F"] = np.column_stack((-sim, hinge))
+    #         else:
+    #             out["F"] = -sim
+
+    #         out["G"] = np.zeros((x.shape[0]))
